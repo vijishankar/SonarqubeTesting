38cf619bb6b0cff2e1a2308ab2c65a959824eec4
viji <vijishankar111@gmail.com>
2020-12-16 11:13:37 +0530

    first commit

---
+++ TestingProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestingProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
